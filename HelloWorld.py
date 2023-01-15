65e30054becb679e339d698ea20087a9f2dbd2e9
Ashar Alvany <ashzee10000@gmail.com>
2023-01-14 18:32:32 -0600

    minor changes

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,8 @@
 print("Hello World")
 # add - commit - push
+
+# section 1 - working on dashboard
+print("this is the new Dashboard")
+
+# section 2 - working on patient aesthetic
+print("This is the updated patient view") 
